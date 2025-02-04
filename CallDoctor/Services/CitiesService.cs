2b07434f1539b62c5e21e28ea8b52fabae8d4fc5
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-05 00:04:38 +0200

    Add 'UploadCitiesFromExcel' Action Method

--- CallDoctor/Services/CitiesService.cs
+++ CallDoctor/Services/CitiesService.cs
@@ -1,6 +1,11 @@
 ﻿using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using OfficeOpenXml;
 using ServiceConstracts;
 using ServiceConstracts.DTO;
+using Services.Helpers;
+using System.ComponentModel;
 
 namespace Services
 {
@@ -12,9 +17,18 @@ namespace Services
         {
             _db = doctorsDbContext;
         }
-        public Task<CityResponse> AddCity(CityAddRequest cityAddRequest)
+        public async Task<CityResponse>? AddCity(CityAddRequest? cityAddRequest)
         {
-            throw new NotImplementedException();
+           if(cityAddRequest == null) throw new ArgumentNullException(nameof(cityAddRequest));
+           ValidationHelper.ModelValidation(cityAddRequest);
+           if(_db.Cities.Where(temp => temp.CityName == cityAddRequest.CityName).Count()> 0)
+            {
+                throw new ArgumentException("The given City is already exists");
+            }
+           City city = new City() { CityName = cityAddRequest.CityName , CityId = Guid.NewGuid()};
+           _db.Cities.Add(city);
+           await _db.SaveChangesAsync();
+           return city.ToCityResponse();
         }
 
         public async Task<List<CityResponse>> GetAllCities()
@@ -26,6 +40,33 @@ namespace Services
         public Task<CityResponse> GetCityById(Guid id)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<int> UploadCitiesFromExcel(IFormFile formFile)
+        {
+            MemoryStream stream = new MemoryStream();
+            await formFile.CopyToAsync(stream);
+            int citiesInserted = 0;
+            using(ExcelPackage excelPackage = new ExcelPackage(stream))
+            {
+               ExcelWorksheet excelWorksheet = excelPackage.Workbook.Worksheets["Cities"];
+                int rowNumber = excelWorksheet.Dimension.Rows;
+                for(int i = 2; i <= rowNumber; i++)
+                {
+                    string? cellValue = Convert.ToString(excelWorksheet.Cells[rowNumber, 1].Value);
+                    if (!string.IsNullOrEmpty(cellValue))
+                    {
+                        string cityName = cellValue;
+                        if (_db.Cities.Where(temp => temp.CityName == cityName).Count() == 0)
+                        {
+                            CityAddRequest cityAddRequest = new CityAddRequest() { CityName = cityName };
+                            await AddCity(cityAddRequest);
+                            citiesInserted++;
+                        }
+                    }
+                }
+            }
+            return citiesInserted;
         }
     }
 }
