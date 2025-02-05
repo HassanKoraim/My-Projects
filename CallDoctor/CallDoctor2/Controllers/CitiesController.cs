379fcd516209c83843a15c31dfb76433cd9f9ff1
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-05 15:27:23 +0200

    Add 'UploadCitiesFromExcel' Action Method

--- CallDoctor/CallDoctor2/Controllers/CitiesController.cs
+++ CallDoctor/CallDoctor2/Controllers/CitiesController.cs
@@ -6,7 +6,7 @@ namespace CallDoctor.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CitiesController : ControllerBase
+    public class CitiesController : Controller
     {
         private readonly ICitiesService _citiesService;
 
@@ -30,7 +30,29 @@ namespace CallDoctor.Controllers
                 return NotFound();
             return Ok(city);
         }
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> UploadCitiesFromExcel()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> UploadCitiesFromExcel(IFormFile formFile)
+        {
+            if(formFile == null || formFile.Length == 0)
+            {
+                ViewBag.ErrorMessage = "Please an xlsx File";
+            }
+            if (!Path.GetExtension(formFile.FileName).Equals(".xlxs", StringComparison.OrdinalIgnoreCase))
+            {
+                ViewBag.ErrorMessage = "Unsupported file. 'xlsx' is expected";
+                return View();
+            }
+            ViewBag.CitiesInsertes = await _citiesService.UploadCitiesFromExcel(formFile);
+            ViewBag.Message = $"{@ViewBag.CitiesInsertes} Cities Inserted Sucessfully";
+            return View();
+        }
 
-        
     }
 }
