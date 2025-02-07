b289ee1a27c737c22b89132de4790f0341ce0840
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-07 12:05:39 +0200

    Add Some changes and 'GetAllDoctors' Stored Procedure

--- CallDoctor/Entities/Migrations/20250207090448_AddSomechanges.Designer.cs
+++ CallDoctor/Entities/Migrations/20250207090448_AddSomechanges.Designer.cs
@@ -4,6 +4,7 @@ using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Entities.Migrations
 {
     [DbContext(typeof(DoctorsDbContext))]
-    partial class DoctorsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250207090448_GetDoctors-storedProcedure")]
+    partial class AddSomechanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CallDoctor/Entities/Migrations/20250207091003_GetAllDoctors-storedProcedure.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    public partial class GetAllDoctorsstoredProcedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string sp_GetAllDoctors =
+                @" CREATE PROCEDURE [dbo].[GetAllDoctors]
+                   AS BEGIN
+                   SELECT DoctorId, DoctorName, CityName, CityId, Address,
+                   PhoneNumber, ExaminationPrice,Specialization
+                   FROM [dbo].[Doctors]
+                   END ";
+           migrationBuilder.Sql(sp_GetAllDoctors);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string sp_GetAllDoctors =
+                @"DROP PROCEDURE [dbo].[GetAllDoctors]";
+            migrationBuilder.Sql(sp_GetAllDoctors);
+        }
+    }
+}
